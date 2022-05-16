05deb7c4ff588f513a2b4985af6417706d2ced6d
hniz <hammamzahran@gmail.com>
2022-05-16 13:09:35 +0200

    adding the loot drop effect

---
+++ CompanyTask/Assets/States_Effect/Scripts/LootDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class LootDrop : MonoBehaviour
+{
+    public GameObject lootObject;
+    public VisualEffect lootVFX;
+
+    void Start()
+    {
+        lootVFX.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider co)
+    {
+        if(co.gameObject.tag == "Player")
+        {
+            lootVFX.Stop();
+            lootObject.SetActive(false);
+            Destroy (gameObject, 5);
+        }
+
+    }
+}
